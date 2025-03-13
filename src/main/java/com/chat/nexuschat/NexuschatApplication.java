28534e21d448a14e192d539df9b3e8e40bdf4fa2
sidraaslam2110@gmail.com <sidraaslam2110@gmail.com>
2025-03-13 20:11:43 +0300

    complete app

---
+++ src/main/java/com/chat/nexuschat/NexuschatApplication.java
@@ -0,0 +1,13 @@
+package com.chat.nexuschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexuschatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexuschatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chat/nexuschat/NexuschatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.nexuschat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexuschatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
